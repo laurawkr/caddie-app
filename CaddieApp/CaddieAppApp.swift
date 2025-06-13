77ee54926af17ae0058271b022a83ad6c3032050
laurawkr <laurawhicker@gmail.com>
2025-06-13 12:18:57 -0700

    Initial Commit

---
+++ CaddieApp/CaddieAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CaddieAppApp.swift
+//  CaddieApp
+//
+//  Created by Laura Whicker on 6/13/25.
+//
+
+import SwiftUI
+
+@main
+struct CaddieAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
